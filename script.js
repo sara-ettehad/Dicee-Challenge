b1ef466cdaf33ad012c818c13dad84573d7a1e87
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2023-03-13 22:28:00 -0400

    message

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function randomNumber1(){
-    let randomDice = Math.random();
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let src1 = "images/" + "dice" + randomNumber1 + ".png";
+let src2 = "images/" + "dice" + randomNumber2 + ".png";
+
+let p1 = "player1";
+let p2 = "player2";
+
+document.querySelectorAll("img")[0].setAttribute("src" , src1);
+document.querySelectorAll("img")[1].setAttribute("src" , src2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML= p1 + "wins!";
 }
+
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = p2 + " wins!";
+}
+
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
